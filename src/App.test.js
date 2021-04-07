b9015368546368277b87b30f26d7e987fdf2a609
alexlecco <alexlecco24@gmail.com>
2021-04-07 11:24:08 -0300

    write first test in enzyme shallow way

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+describe('Counter testing', () => {
+  test('renders the title of counter', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toContain(
+      'This is a counter app'
+    );
+    // old version:
+    // render(<App />);
+    // const linkElement = screen.getByText('This is a counter app');
+    // expect(linkElement).toBeInTheDocument();
+  });
 });
