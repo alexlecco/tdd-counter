2186d00a19ec14d38ea66dea4dfb40352ef89e85
alexlecco <alexlecco24@gmail.com>
2021-04-07 12:18:41 -0300

    write t3 with initial state value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>This is a counter app</h1>
+        <div id="counter-value">{counter}</div>
         <button id="increment-btn">Increment</button>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -24,4 +24,8 @@ describe('Counter testing', () => {
   test('t2: render a button with text of `increment`', () => {
     expect(wrapper.find('#increment-btn').text()).toBe('Increment');
   });
+
+  test('t3: render initial value of state in a div', () => {
+    expect(wrapper.find('#counter-value').text()).toBe('0');
+  });
 });
